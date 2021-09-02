1ffac7c0bcf253145f4c554788970e91dc5c0e89
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-09-02 13:21:22 -0700

    Update WorldGen.cpp

--- WorldGen.cpp
+++ WorldGen.cpp
@@ -1,16 +1,4 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <string> 
-
-//Length is the length of the array 
-//width is the width of the array
-//Denisty is the number of hills/noticable height diffrences
-//SpreadChancex is the chance for it to spread in the x axis
-//SpreadChancey is the chance for it to spread in the y axis
-//Runtime is the amount of times it adds to the hills recommended is 10 - 30 depending on how much hill you want
-void CreateHeightMap(int length, int width, int Density, int SpreadChancex, int SpreadChancey, int RunTime){
-  SpreadChancex += SpreadChancey;
+void CreateHeightMap(int length, int width, int Density, int RunTime, int jagged){
   int HeightMap[length][width];
   for(int i=0;i<length;i++){
     for(int i1=0;i1<width;i1++){
@@ -27,37 +15,30 @@ void CreateHeightMap(int length, int width, int Density, int SpreadChancex, int 
       for(int i2=0;i2<width;i2++){
         if(HeightMap[i1][i2] >= 1){
           int ran = (rand() % 100) + 1;
-          SpreadChancey -= HeightMap[i1][i2]-1;
-          if(ran >= SpreadChancey && ran <= SpreadChancex){
             int ran1 = (rand() % 4) +1;
             if(i1 -1 >= 0 && i1 +1 < length && i2 -1 >= 0 && i2 +1 < width){
               if(ran1 == 1){
                 if(HeightMap[i1-1][i2] <= HeightMap[i1][i2] + 1){
-                  HeightMap[i1-1][i2] +=1;
+                  HeightMap[i1-1][i2] +=jagged;
                 }
               }
               if(ran1 == 2){
                 if(HeightMap[i1+1][i2] <= HeightMap[i1][i2] + 1){
-                  HeightMap[i1+1][i2] +=1;
+                  HeightMap[i1+1][i2] +=jagged;
                 }
               }
               if(ran1 == 3){
                 if(HeightMap[i1][i2-1] <= HeightMap[i1][i2] + 1){
-                  HeightMap[i1][i2-1] +=1;
+                  HeightMap[i1][i2-1] +=jagged;
                 }
               }
               if(ran1 == 4){
                 if(HeightMap[i1][i2+1] <= HeightMap[i1][i2] + 1){
-                  HeightMap[i1][i2+1] +=1;
+                  HeightMap[i1][i2+1] +=jagged;
                 }
               }
-            }
-          }
-          if(ran <= SpreadChancey){
-             HeightMap[i1][i2] +=1;
           }
         }
       }
     }
-  } 
-}
+  }
